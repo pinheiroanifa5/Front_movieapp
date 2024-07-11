c2c650347ff3a6b2b32007a568c3c27a94fac1d2
pinheiroanifa5 <anifapinheiro5@gmail.com>
2024-07-11 12:56:40 -0100

    admin buttons

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -2,13 +2,14 @@ import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteUser, fetchAllUsers } from '../store/reducers/UsersSlice';
-import { useNavigate } from 'react-router-dom';
+import { AiOutlineArrowLeft } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 
 const AdminPage = () => {
     const [loading, setLoading] = useState(true);
     const dispatch = useDispatch();
     const users = useSelector((state) => state.users.users);
-    const token = localStorage.getItem("token") // Certifique-se de que o token está sendo armazenado no estado auth
+    const token = localStorage.getItem("token"); // Certifique-se de que o token está sendo armazenado no estado auth
 
     useEffect(() => {
         if (token) {
@@ -16,16 +17,14 @@ const AdminPage = () => {
                 await dispatch(fetchAllUsers(token)).then(() =>
                     setLoading(false)
                 );
-
             };
             loadUsers();
         }
     }, [dispatch, token]);
 
-
     const handleDeleteUser = (user) => {
-        dispatch(deleteUser(token, user))
-    }
+        dispatch(deleteUser(token, user));
+    };
 
     if (loading) {
         return <p>Loading...</p>;
@@ -33,6 +32,11 @@ const AdminPage = () => {
 
     return (
         <Container>
+            <TopBar>
+                <Link to="/" className="back-link">
+                    <AiOutlineArrowLeft /> Voltar
+                </Link>
+            </TopBar>
             <h1>Registered Users</h1>
             <UserList>
                 {users.map((user) => (
@@ -41,6 +45,7 @@ const AdminPage = () => {
                         <p><strong>Name:</strong> {user.name}</p>
                         <p><strong>Email:</strong> {user.email}</p>
                         <p><strong>Role:</strong> {user.role}</p>
+                        <Button onClick={() => handleDeleteUser(user)}>Remover</Button>
                     </UserItem>
                 ))}
             </UserList>
@@ -50,6 +55,29 @@ const AdminPage = () => {
 
 const Container = styled.div`
   padding: 2rem;
+  background-color: #222;
+  min-height: 100vh;
+  color: #fff;
+`;
+
+const TopBar = styled.div`
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  background-color: #141414;
+  padding: 1rem;
+  color: white;
+
+  .back-link {
+    display: flex;
+    align-items: center;
+    color: red;
+    text-decoration: none;
+
+    svg {
+      margin-right: 0.5rem;
+    }
+  }
 `;
 
 const UserList = styled.div`
@@ -62,6 +90,20 @@ const UserItem = styled.div`
   padding: 1rem;
   border: 1px solid #ddd;
   border-radius: 4px;
+`;
+
+const Button = styled.button`
+  background-color: #e50914;
+  color: #fff;
+  border: none;
+  padding: 0.5rem 1rem;
+  border-radius: 4px;
+  cursor: pointer;
+  font-size: 1rem;
+
+  &:hover {
+    background-color: #f6121d;
+  }
 `;
 
 export default AdminPage;
